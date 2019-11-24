c5007542763935aa766eb7ab06237b3006154e7d
Voovs Selin <Voovs.13@gmail.com>
2019-11-23 18:20:25 -0700

    2

--- src/main.js
+++ src/main.js
@@ -3,16 +3,6 @@ import ErrorPage from './views/error-page.js';
 import Home from './views/home.js';
 import ColorSchemer from './views/color-schemer.js';
 
-<<<<<<< Updated upstream
-function rotate_theme () {
-  console.log("fired")
-  let prev_theme_index = curr_theme_index
-  if (curr_theme_index === 2) curr_theme_index = 0
-  else curr_theme_index += 1
-
-  document.querySelector("#body").classList.replace(themes[prev_theme_index], themes[curr_theme_index])
-}
-=======
 const routes = [
   { path: '/', component: Home },
   { path: '/color-schemer', component: ColorSchemer },
@@ -33,4 +23,3 @@ const app = new Vue ({
 // new Vue ({
 //   render: h => h(App),
 // }).$mount(`#app`);
->>>>>>> Stashed changes
