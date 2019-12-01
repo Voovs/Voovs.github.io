c0c9040c78a819a361f0d1f1325fca8054275a63
Voovs <Voovs.13@gmail.com>
2019-11-30 23:55:40 -0700

    Finished js for power-search
    - Methods and computed now word
    - Still need to fix how it takes in a 2d json array/object

---
+++ src/assets/theater-tropes.js
@@ -0,0 +1,308 @@
+﻿var theater_tropes = [
+ {
+   "Trope": "One of the boys",
+   "Type": "Character",
+   "Description": "A girl who is introduced as \"in\" with the guys ",
+   "Example": "Hermione Granger at the Yule Ball"
+ },
+ {
+   "Trope": "Word of Gay",
+   "Type": "",
+   "Description": "When a creator of a piece reveals one of the characters, who's sexuallity wasn't mentioned, is actually gay or LGBTQ",
+   "Example": "Albus Dumbledore being forced out of the closet by JK Rowling"
+ },
+ {
+   "Trope": "Comically missing the point",
+   "Type": "Comedy, plot, character",
+   "Description": "Taking away the wrong idea from a piece of information, which leads to an amusing misunderstanding",
+   "Example": ""
+ },
+ {
+   "Trope": "Grammar nazi",
+   "Type": "Character",
+   "Description": "",
+   "Example": ""
+ },
+ {
+   "Trope": "Gay bravado",
+   "Type": "Character, comedy",
+   "Description": "When a straight guy makes really homosexual comments, which serve to only boost their straightness. Unconventional, yet might work for girls",
+   "Example": ""
+ },
+ {
+   "Trope": "Real men wear pink",
+   "Type": "Comedy",
+   "Description": "When a guy is so clearly masculine, and confident in their masculinity, they end up with one of two girly traits",
+   "Example": ""
+ },
+ {
+   "Trope": "Wrench wench",
+   "Type": "Character",
+   "Description": "When a traditionally masculine job is taken on by an otherwise feminine character. Doesn't really work for LGBTQ",
+   "Example": ""
+ },
+ {
+   "Trope": "Please put some clothes on",
+   "Type": "Comedy",
+   "Description": "When one character walks in on another character who's naked",
+   "Example": "Sherlock not putting any clothes on for the Buckingham palace"
+ },
+ {
+   "Trope": "Valley girl (modern: VSCO girl)",
+   "Type": "Stock character, comedy",
+   "Description": "A character who's, like, totes self-interested and SOOO saw that cheating b—— with Jacob yesterday",
+   "Example": ""
+ },
+ {
+   "Trope": "Uncanny valley girl",
+   "Type": "Character/deity",
+   "Description": "A character who's essentially a deity, though not alpha b——, and is liked by everyone. Usually ends up having a secret malicious intent",
+   "Example": "Brenna???"
+ },
+ {
+   "Trope": "That sounds fake",
+   "Type": "Character, comedy, 🔥🔥🔥",
+   "Description": "When a deity deconstructs a character and then insults them using their most vulnerable point",
+   "Example": "Brenna's \"That sounds fake\""
+ },
+ {
+   "Trope": "Those two guys",
+   "Type": "Character",
+   "Description": "Two guys that are usually best friends. They'll have a few sharp contrasts and be identical otherwise",
+   "Example": ""
+ },
+ {
+   "Trope": "Dark horse",
+   "Type": "Character",
+   "Description": "Minor character who gains surprising popularity with the audience",
+   "Example": "Sandor Clegane being popular enough to be brought back"
+ },
+ {
+   "Trope": "Dark horse victory",
+   "Type": "Plot",
+   "Description": "An otherwise unmentioned character coming out ahead of 2 major rivals",
+   "Example": ""
+ },
+ {
+   "Trope": "Armoured closet gay",
+   "Type": "Character",
+   "Description": "Someone who is gay, yet acts super straight and doesn't admit to being gay. Ends up a bit paradoxical",
+   "Example": ""
+ },
+ {
+   "Trope": "Girly girl with a tomboy streak",
+   "Type": "Character",
+   "Description": "A girl whose interests are mostly feminine, yet have masculine outliers. Somewhat similar to \"real men wear pink\"",
+   "Example": ""
+ },
+ {
+   "Trope": "An aesop",
+   "Type": "Structure",
+   "Description": "End line of a piece essentially summarizes the anvil theme",
+   "Example": ""
+ },
+ {
+   "Trope": "Camp straight",
+   "Type": "Character",
+   "Description": "A straight guy who acts gay, yet has a trait that makes them clearly straight",
+   "Example": ""
+ },
+ {
+   "Trope": "Ambiguously gay",
+   "Type": "Character",
+   "Description": "Characters who seem to be gay, in their actions and choices, yet nobody in the universe explicitly states their sexual orientation",
+   "Example": ""
+ },
+ {
+   "Trope": "The un-reveal",
+   "Type": "Plot",
+   "Description": "When it looks like a reveal is coming and then it doesn't come. Could be comedic or frustrating",
+   "Example": "Deadpool's mask coming off, only to have another mask underneath!"
+ },
+ {
+   "Trope": "Emerging from the shadows",
+   "Type": "Plot, character",
+   "Description": "After being concealed in shadow, the character suddenly steps out for the audience to recognize",
+   "Example": "Captain America stepping out in that Infinity War subway scene"
+ },
+ {
+   "Trope": "Missing the good stuff",
+   "Type": "Plot",
+   "Description": "When a character misses a very important part of an event, which creates rising action",
+   "Example": "Romeo misses the messenger's message that Juliet isn't dead for real"
+ },
+ {
+   "Trope": "Tempting fate",
+   "Type": "Plot",
+   "Description": "Being way too certain about things, only to have them go downhill unexpectedly",
+   "Example": "Othello being all \"my life upon her faith\" and overly confident about Desdemona"
+ },
+ {
+   "Trope": "Out the frying pan",
+   "Type": "Plot",
+   "Description": "Narrowly escaping from one bad situation, only to hit an even worse one",
+   "Example": "Harry and friends escape from Flinch in the third floor corridor... only to find a 3 headed dog in the same room"
+ },
+ {
+   "Trope": "Nice job breaking it hero",
+   "Type": "Plot",
+   "Description": "When a protagonist succeeds in their goal, only to have that goal turn out to make things worse",
+   "Example": ""
+ },
+ {
+   "Trope": "Hero antagonist",
+   "Type": "Character, plot",
+   "Description": "An antagonist, in the sense that they oppose the protagonist, yet their objective is in line with the protagonists",
+   "Example": "Infinity Blade II, when you defeat the final boss... who was just keeping the world safe"
+ },
+ {
+   "Trope": "Opposing sports team",
+   "Type": "Character",
+   "Description": "Often a team which is currently number 1, through means of cheating or immoral play. They play the game solely to win",
+   "Example": "Slytherin Quidditch team"
+ },
+ {
+   "Trope": "Technician versus performer",
+   "Type": "Character, plot",
+   "Description": "Juxtaposition of one highly skilled individual and one less skilled yet more dedicated one. Common in human activities",
+   "Example": ""
+ },
+ {
+   "Trope": "Colour coded for your convenience",
+   "Type": "Symbol, structure",
+   "Description": "Using visuals, like colour, to give audiences instantaneous information about a situation",
+   "Example": ""
+ },
+ {
+   "Trope": "Red oni blue oni",
+   "Type": "Character, plot",
+   "Description": "Two linked characters which have fundamental disagreements about something. Red often being wilder, while blue is more serene",
+   "Example": ""
+ },
+ {
+   "Trope": "Defeat means friendship",
+   "Type": "Plot, character",
+   "Description": "Upon defeat, the antagonist shows so much remorse, they change their perspective to be in line with the protagonist",
+   "Example": "Draco Malfoy progressively moving over to Potter's side"
+ },
+ {
+   "Trope": "Kick the dog",
+   "Type": "Character",
+   "Description": "An act to cruel or repugnant, the character committing it is instantly vilified beyond doubt",
+   "Example": ""
+ },
+ {
+   "Trope": "Pet the dog",
+   "Type": "Character",
+   "Description": "When an otherwise immoral character is shown doing something good. Gives them a positive side as well",
+   "Example": ""
+ },
+ {
+   "Trope": "Sympathetic POV",
+   "Type": "Structure, character",
+   "Description": "The effect which creates an protagonist and makes their moral choices generally correct. Important in ambiguous moral situations",
+   "Example": "ASOIAF constantly changes the sympathetic point of view to make everyone a situational protagonist"
+ },
+ {
+   "Trope": "Cuteness overload",
+   "Type": "Plot",
+   "Description": "A character encountering something so cute, they're temporarily somewhat incapacitated",
+   "Example": ""
+ },
+ {
+   "Trope": "Pick up babes with babes",
+   "Type": "Plot",
+   "Description": "A guy who's taking care of a young child or baby is suddenly upgraded in attractiveness to girls",
+   "Example": ""
+ },
+ {
+   "Trope": "Animal chick magnet",
+   "Type": "Plot",
+   "Description": "A guy who's great with animals is suddenly upgraded in attractiveness to girls",
+   "Example": ""
+ },
+ {
+   "Trope": "A simple plan",
+   "Type": "Plot",
+   "Description": "When a character sets up a really straightforward plan, which fails due to themselves or something unexpected",
+   "Example": "\"I'll be the first to admit that this did not go according to plan. I'll also be the first to admit that the plan was written in crayon\" -deadpool 2"
+ },
+ {
+   "Trope": "Time for plan B",
+   "Type": "Plot, comedy",
+   "Description": "A stock phrase in which there's a backup plan. Often the backup has not been thought through",
+   "Example": ""
+ },
+ {
+   "Trope": "Fawlty towers plot",
+   "Type": "Plot",
+   "Description": "When a character tells one lie, which then leads to more cover up lies and eventually all the lies come crashing down",
+   "Example": ""
+ },
+ {
+   "Trope": "Fluffy tamer",
+   "Type": "Character",
+   "Description": "A character who really likes pets, yet these pets are clearly dangerous",
+   "Example": "Hagrid's taming of Fluffy and Aragog"
+ },
+ {
+   "Trope": "Villain protagonist",
+   "Type": "Character",
+   "Description": "When your protagonist is clearly in the moral wrong and you support them anyways",
+   "Example": ""
+ },
+ {
+   "Trope": "Punch clock villain",
+   "Type": "Character",
+   "Description": "A character who's only a villian on their job. Usually their boss causes the villainy. Sort of ambiguous villainous",
+   "Example": ""
+ },
+ {
+   "Trope": "Cursed with awesome",
+   "Type": "Character, plot",
+   "Description": "When a character gets a 'curse', usually uncontrollable, which is actually pretty awesome",
+   "Example": "Edward Collin is hot, immortal, and super strong, yet whines about being a vampire"
+ },
+ {
+   "Trope": "Compliment fishing",
+   "Type": "Plot, character",
+   "Description": "A character saying some aspect is bad, to get another character's deminal and therefore compliments",
+   "Example": "Aneca: \"I'm not good at physics\", Voovs: ???"
+ },
+ {
+   "Trope": "Meaningful name",
+   "Type": "Character",
+   "Description": "When a character's name is symbolic is some way",
+   "Example": "Jasmine from Coping Mechanism is anviliciously named after a Disney princess"
+ },
+ {
+   "Trope": "All that glitters",
+   "Type": "Symbol",
+   "Description": "",
+   "Example": ""
+ },
+ {
+   "Trope": "Brick joke",
+   "Type": "Comedy, structure",
+   "Description": "Idea of stating half a joke at one point and ending it much later. Uses callback. Sort of like a plant seed",
+   "Example": ""
+ },
+ {
+   "Trope": "I am what I am",
+   "Type": "Character, plot",
+   "Description": "A character embraces something about themselves they previously saw a character flaw",
+   "Example": "\"I'm not a big fat panda. I'm THE big fat panda\" -Po"
+ },
+ {
+   "Trope": "Oh crap",
+   "Type": "Plot",
+   "Description": "When a character realizes something really bad is about to happen. Usually has a shocked expression to reveal their recognition",
+   "Example": ""
+ },
+ {
+   "Trope": "One man army",
+   "Type": "Character",
+   "Description": "One character who's fighting like they're an entire army",
+   "Example": ""
+ }
+]
--- src/components/power-search.js
+++ src/components/power-search.js
@@ -6,13 +6,13 @@ export default {
       <span class="search-bar"><input type="text" v-model="user_search"></span>
     </div>
     <div class="results-parent">
-      <div class="card" v-for="word_data in words_data"  :key="word_data.word">
-        <div class="icon">{{ word_data.type }}</div>
+      <div class="card" v-for="index in display_words_index" :key="words_data[index].trope">
+        <div class="icon">{{ words_data[index].type }}</div>
         <div class="text-content">
-          <h1 class="word-title">{{ word_data.word }}</h1>
-          <p class="word-description">{{ word_data.description }}</p>
+          <h1 class="word-title">{{ words_data[index].trope }}</h1>
+          <p class="word-description">{{ words_data[index].description }}</p>
         </div>
-        <!-- <div class="rating">
+        <!-- <div class="tags">
 
         </div> -->
       </div>
@@ -27,27 +27,97 @@ export default {
         { word: "Bravado", type: "Juciy", description: "Boldness intended to please" },
         { word: "Callback", type: "Theatrical", description: "Referencing a past event for its emotion" },
       ],
-      display_words_index = [], //Index of words to be displayed from words_data
+      display_words_index: [], //Index of words to be displayed from words_data
       user_search: ""
     }
   },
-  watch: {
-    user_search: () => {
+  computed: {
+    // Map with key: index, value: string of word+type+description
+    sum_string_map () {
+      let sum_string_map = new Map()
+      let index = 0
 
+      for (let item of this.words_data) {
+        let sum_string =`${item.word.toLowerCase()} ${item.type.toLowerCase()} ${item.description.toLowerCase()}`
+
+        sum_string_map.set(index, sum_string)
+
+        index += 1
+      }
+
+      return sum_string_map
+    },
+    search_is_blank () {
+      let search_char_arr = this.user_search.split('')
+
+      for (let char of search_char_arr) {
+        if (char.match(/[a-z]/gi)) return false
+      }
+      return true
+    },
+    valid_search_arr () {
+      let search_word_arr = this.user_search.toLowerCase().split(' ') // Lowercase array
+      let search_char_arr = [] // 2D array of [word][char]
+
+      for (let word of search_word_arr) {
+        search_char_arr.push(word.split (''))
+      }
+
+      search_word_arr = []
+
+      for (let char_arr of search_char_arr) {
+        search_word_arr.push(
+          char_arr
+          .filter(char => char.match(/[a-z]/gi))
+          .toString()
+          .replace(/\,/gi, '')
+        )
+      }
+
+      search_word_arr = search_word_arr.filter(word => word !== '')
+
+      return search_word_arr // array of strings
+    }
+  },
+  watch: {
+    user_search: function () {
+      this.search_all_words()
     }
   },
   methods: {
     search_all_words () {
-      let user_search = this.user_search
+      if (this.search_is_blank) {
+        this.display_words_index = [...Array(this.words_data.length).keys()]
 
-      if (user_search === '' || input.match(/[\/\(\)\[\]\{\}\*\^\$\!\@\?\.\|\+\\]/gi)) {
-        this.display_words_index = []
       } else {
+        this.display_words_index = []
 
+        console.log(this.valid_search_arr)
+
+        for (let [key, value] of this.sum_string_map) {
+
+          for (let search of this.valid_search_arr) {
+
+            if (value.match(search))  this.display_words_index.push(key)
+          }
+        }
       }
-    }
-  }
+
+      let remove_duplicates = new Set(this.display_words_index)
+
+      this.display_words_index = Array.from(remove_duplicates)
+    },
+  },
+  created () {
+    this.words_data = window.theater_tropes
+  },
+  mounted () {
+    this.display_words_index = [...Array(this.words_data.length).keys()]
+    console.log(this.display_words_index)
+    console.log(window.theater_tropes)
+    console.log(this.words_data[0])
+  },
+}
   // props: {
   //   word_data: Object
   // }
-}
